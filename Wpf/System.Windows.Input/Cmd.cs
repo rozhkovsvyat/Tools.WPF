d8472dfb420b8d668ab4cc1599fb6e82ee54dd58
Svyat Rozhkov <rozhkovsvyat@gmail.com>
2023-10-27 14:24:53 +0700

    Добавьте файлы проекта.

---
+++ Wpf/System.Windows.Input/Cmd.cs
@@ -0,0 +1,46 @@
+﻿namespace System.Windows.Input;
+
+/// <summary>
+/// Command.
+/// </summary>
+public class Cmd : ICommand
+{
+	#region ICommand
+
+	/// <inheritdoc/>
+	public bool CanExecute(object? parameter) 
+		=> _canExecute == null || _canExecute(parameter);
+
+	/// <inheritdoc/>
+	public void Execute(object? parameter) 
+		=> _execute(parameter);
+
+	/// <inheritdoc/>
+    public event EventHandler? CanExecuteChanged
+    {
+	    add => CommandManager.RequerySuggested += value;
+	    remove => CommandManager.RequerySuggested -= value;
+    }
+
+	#endregion
+
+	/// <summary>
+	/// Method that determines whether the command can execute in its current state.
+	/// </summary>
+	private readonly Func<object?, bool>? _canExecute;
+	/// <summary>
+	/// Method to be called when the command is invoked.
+	/// </summary>
+	private readonly Action<object?> _execute;
+
+	/// <summary>
+	/// Constructor.
+	/// </summary>
+	/// <param name="execute">Method to be called when the command is invoked.</param>
+	/// <param name="canExecute">Method that determines whether the command can execute in its current state.</param>
+	public Cmd(Action<object?> execute, Func<object?, bool>? canExecute = null)
+	{
+		_execute = execute; 
+		_canExecute = canExecute;
+	}
+}
